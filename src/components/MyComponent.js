49116c01343a21d45580a7920f21e1a10b9b828d
Mif Two EggS <hieuv6653@gmail.com>
2023-03-16 23:31:00 +0700

    check code day 1

---
+++ src/components/MyComponent.js
@@ -0,0 +1,14 @@
+// class component
+// function component
+import React from "react";
+class MyComponents extends React.Component {
+    render() {
+        return (
+            <div>
+                Hello MyComponents
+            </div>
+        );
+    }
+}
+
+export default MyComponents;
