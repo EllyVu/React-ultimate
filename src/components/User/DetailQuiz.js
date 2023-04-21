e6c9acc3cd2d391c5ed037cf6d7b7843a00dbf2c
Mif Two EggS <hieuv6653@gmail.com>
2023-04-21 22:49:20 +0700

    init project

--- src/components/User/DetailQuiz.js
+++ src/components/User/DetailQuiz.js
@@ -1,10 +1,13 @@
 import { useEffect } from "react";
-import { useParams } from "react-router-dom"
+import { useParams, useLocation } from "react-router-dom"
 import { getDataQuiz } from '../../Services/apiServices'
 import _ from "lodash"
+import "./DetailQuiz.scss"
 
 const DetailQuiz = (props) => {
     const params = useParams();
+    const location = useLocation();
+
     const quizId = params.id;
 
     useEffect(() => {
@@ -36,7 +39,33 @@ const DetailQuiz = (props) => {
     }
     return (
         <div className="detail-quiz-container">
-            DetailQuiz
+            <div className="left-content">
+                <div className="title">
+                    Quiz {quizId} :  {location?.state?.quizTitle}
+                </div>
+                <hr />
+                <div className="question-body">
+                    <img />
+                </div>
+                <div className="question-content">
+                    <div className="questions">
+                        Question 1:   How are u Doing ???
+                    </div>
+                    <div className="answer">
+                        <div className="a-child">A. câu trả lời 1 </div>
+                        <div className="b-child">B. câu trả lời 2</div>
+                        <div className="c-child">C. câu trả lời 3</div>
+                    </div>
+                </div>
+                <div className="footer">
+                    <button className="btn btn-warning">Prev</button>
+                    <button className="btn btn-info">Next</button>
+                </div>
+            </div>
+            <div className="right-content">
+                count down
+
+            </div>
         </div>
     )
 }
